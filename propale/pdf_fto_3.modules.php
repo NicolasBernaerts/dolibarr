18251d994283b80ef25b825a2f03ccfbd6d7af78
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-01-16 20:00:03 +0100

    Add Dolibarr 3.8 compatibility

--- propale/pdf_fto_3.modules.php
+++ propale/pdf_fto_3.modules.php
@@ -1,10 +1,10 @@
 <?php
-/* Copyright (C) 2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2005-2012 Regis Houssin        <regis.houssin@capnetworks.com>
- * Copyright (C) 2008      Raphael Bertrand     <raphael.bertrand@resultic.fr>
- * Copyright (C) 2010-2012 Juanjo Menent	    <jmenent@2byte.es>
- * Copyright (C) 2012      Christophe Battarel   <christophe.battarel@altairis.fr>
- * Copyright (C) 2013-2014 Nicolas Bernaerts	<nicolas.bernaerts@gmail.com>
+/* Copyright (C) 2004-2011	Laurent Destailleur	<eldy@users.sourceforge.net>
+ * Copyright (C) 2005-2012	Regis Houssin		<regis.houssin@capnetworks.com>
+ * Copyright (C) 2008		Raphael Bertrand	<raphael.bertrand@resultic.fr>
+ * Copyright (C) 2010-2012	Juanjo Menent		<jmenent@2byte.es>
+ * Copyright (C) 2012		Christophe Battarel	<christophe.battarel@altairis.fr>
+ * Copyright (C) 2013-2016	Nicolas Bernaerts	<nicolas.bernaerts@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,18 +32,19 @@ require_once DOL_DOCUMENT_ROOT.'/core/lib/company.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/pdf.lib.php';
 
+
 /**
  *	Classe permettant de generer les propales au modele Azur
  */
 class pdf_fto_3 extends ModelePDFPropales
 {
 	// FTO - Specific variables. Available models :
-  //  1 - Private or professional from EU traveling to India
-  //  2 - Service to non european travel
-  //  3 - Private outside EU traveling to France
-  //  4 - Service to european professional
-  var $fto_model;
-  var $fto_textCGI;
+	//  1 - Private or professional from EU traveling to India
+	//  2 - Service to non european travel
+	//  3 - Private outside EU traveling to France
+	//  4 - Service to european professional
+	var $fto_model;
+	var $fto_textCGI;
 	var $fto_totalHT;
 	var $fto_priceUHT;
 	// FTO
@@ -65,6 +66,7 @@ class pdf_fto_3 extends ModelePDFPropales
 	var	$marge_basse;
 
 	var $emetteur;	// Objet societe qui emet
+
 
 	/**
 	 *	Constructor
@@ -137,8 +139,8 @@ class pdf_fto_3 extends ModelePDFPropales
 		$this->name        = $langs->transnoentities('FTOName'.$this->fto_model);
 		$this->description = $langs->transnoentities('FTODesc'.$this->fto_model);
 		// Set VAT franchise per model
-    $arrVAT = array(FALSE,FALSE,FALSE,TRUE);
-    $mysoc->tva_assuj = $arrVAT [$this->fto_model - 1];
+		$arrVAT = array(FALSE,FALSE,FALSE,TRUE);
+		$mysoc->tva_assuj = $arrVAT [$this->fto_model - 1];
 		$this->franchise=!$mysoc->tva_assuj;
 		if ($this->franchise) $conf->global->MAIN_GENERATE_DOCUMENTS_WITHOUT_VAT = "1";
 		// Genarate compressed and unencrypted PDF files
@@ -414,7 +416,7 @@ class pdf_fto_3 extends ModelePDFPropales
 					if ((! isset($localtax2_type)) || $localtax2_type=='') $localtax2_type = $localtaxtmp_array[2];
 					//end TODO
 
-				    // retrieve global local tax
+					// retrieve global local tax
 					if ($localtax1_type == '7') $localtax1_rate = $localtaxtmp_array[1];
 					if ($localtax2_type == '7') $localtax2_rate = $localtaxtmp_array[3];
 
@@ -504,7 +506,7 @@ class pdf_fto_3 extends ModelePDFPropales
 
 				// Pied de page
 				$this->_pagefoot($pdf,$object,$outputlangs);
-				$pdf->AliasNbPages();
+				if (method_exists($pdf,'AliasNbPages')) $pdf->AliasNbPages();
 
 				$pdf->Close();
 
